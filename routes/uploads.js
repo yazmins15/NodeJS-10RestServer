9cf921fb5bccf96027a344588e615f52e2362fe2
Yazmin Soriano <yazminsorianocruz@gmail.com>
2024-03-10 22:57:54 -0600

    terminando funcionalidad de carga de imagenes y agregando cloudinary 10/03/2024

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,10 +1,24 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
-const { cargarArchivo } = require('../controllers/uploadsController')
+const { validarCampos, validarArchivoSubir } = require('../middlewares');
+const { cargarArchivo, actualizarImg, mostrarImage, actualizarImgCloud } = require('../controllers/uploadsController');
+const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
 
-router.post('/', cargarArchivo);
+router.post('/', validarArchivoSubir, cargarArchivo);
+
+router.put('/:coleccion/:id', [
+    validarArchivoSubir,
+    check('id', 'El id debe de ser de mongo').isMongoId(),    
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'])),
+    validarCampos
+],  actualizarImgCloud);
+
+router.get('/:coleccion/:id',[
+    check('id', 'El id debe de ser de mongo').isMongoId(),    
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'])),
+    validarCampos
+], mostrarImage);
 
 module.exports = router;
